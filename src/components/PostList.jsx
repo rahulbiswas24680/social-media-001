bf8cad46cb5e36fe181765ca3226d50a5a9aa264
rahulbiswas24680 <rahulbiswas1682001@gmail.com>
2024-11-23 15:28:15 +0530

    useeffect cleanup loading add

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,38 +1,47 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Post from "./Post";
 import { PostList as PostListData } from "../store/post-list-store";
 import WelcomeMessage from "./WelcomeMessage.jsx";
+import LoadingSpinner from "./LoadingSpinner.jsx";
 
 const PostList = () => {
   const { postList, addInitialPosts } = useContext(PostListData);
-  // console.log(postList);
+  const [ fetching, setFetching ] = useState(false);
 
-  const [dataFetched, setDataFetched] = useState(false);
+  useEffect(() => {
+    setFetching(true);
+    console.log('fetch started!!');
 
-  if (!dataFetched) {
-    fetch("https://dummyjson.com/posts")
-        .then((res) => res.json())
-        .then(data => {
-          console.log(data.posts);
-          addInitialPosts(data.posts);
-        });
-
-    setDataFetched(true);
-  }
+    const controller = new AbortController();
+    const signal = controller.signal;
+    
+    fetch("https://dummyjson.com/posts", { signal })
+      .then((res) => res.json())
+      .then(data => {
+        console.log(data.posts);
+        addInitialPosts(data.posts);
+        setFetching(false);
+        console.log('fetch returned!!');
+      });
+    
+    return () => {
+      console.log('Cleaning up useeffect.');
+      controller.abort();
+    }
+  }, []);
 
 
   const handleGetPostClick = () => {
     // console.log("get posts");
-    
+
   };
 
   return (
     <>
       <h2 className="text-center my-4">All Posts</h2>
-      {postList.length === 0 && (
-        <WelcomeMessage onGetPostClick={handleGetPostClick} />
-      )}
-      {postList.map((post) => (
+      { fetching && <LoadingSpinner /> }
+      { !fetching && postList.length === 0 && <WelcomeMessage />}
+      { !fetching && postList.map((post) => (
         <Post key={post.id} post={post}></Post>
       ))}
     </>
--- src/components/WelcomeMessage.jsx
+++ src/components/WelcomeMessage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { IoReload } from "react-icons/io5";
 
-const WelcomeMessage = ({onGetPostClick}) => {
+const WelcomeMessage = () => {
   return (
     <center className="welcome-message">
         <h4>No Posts to show.</h4>
